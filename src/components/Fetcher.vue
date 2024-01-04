e74f901a4c778d28bdca20fbfa0db012e566cab9
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-01-04 10:48:03 +0545

    add styling in fetching component

--- src/components/Fetcher.vue
+++ src/components/Fetcher.vue
@@ -34,8 +34,8 @@ const processData = (response: any) => {
     joke.value = response.joke;
     jokeType.value = type;
   } else if (type === "twopart") {
-    setup.value = response.setup;
-    delivery.value = response.delivery;
+    setup.value = response?.setup;
+    delivery.value = response?.delivery;
     jokeType.value = type;
   } else {
     resetValues();
@@ -43,7 +43,7 @@ const processData = (response: any) => {
   }
 };
 
-const fetchData = async () => {
+const fetchData = async (): Promise<JokeResponse> => {
   loading.value = true;
   const response = await fetch(props.url);
   const data = await response.json();
@@ -53,15 +53,16 @@ const fetchData = async () => {
 
 const handleClick = async () => {
   const fetchedData = await fetchData();
-  console.log(fetchedData);
   processData(fetchedData);
 };
 </script>
 
 <template>
   <button @click="handleClick">Fetch a joke!</button>
-  {{ url }}
-  <p v-if="loading">Loading...</p>
+  <div class="url-container">
+    {{ url }}
+  </div>
+  <p class="loading-text" v-if="loading">Loading...</p>
   <p v-else-if="jokeType === 'single'">
     {{ joke }}
   </p>
@@ -73,7 +74,40 @@ const handleClick = async () => {
 </template>
 
 <style scoped>
+.url-container {
+  border: 1px solid white;
+  border-radius: 8px;
+  padding: 0.5rem 1rem;
+}
+
 p {
   text-align: center;
+}
+
+.loading-text {
+  color: red;
+  font-weight: bold;
+}
+
+button {
+  padding: 0.5rem 1rem;
+  font-size: inherit;
+  background-color: green;
+  color: white;
+  border-radius: 16px;
+  border: none;
+  outline: none;
+  transition: all 0.8s ease;
+
+  &:hover,
+  &:focus {
+    cursor: pointer;
+    background-color: limegreen;
+  }
+
+  &:focus {
+    outline-offset: 2px;
+    outline: 1px solid green;
+  }
 }
 </style>
